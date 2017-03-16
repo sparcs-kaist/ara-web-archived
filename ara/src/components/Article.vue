66b9e56aaacab9206281d634911ef1434da5c4d1
Kang-in Raon Kim <talkwithraon@gmail.com>
2017-03-16 23:27:05 +0900

    샘플 코드

---
+++ ara/src/components/Article.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="ui container">
+    <div class="ui comments">
+      <h3 class="ui dividing header">Comments</h3>
+      <div class="comment">
+        <a class="avatar">
+          <img src="/images/avatar/small/matt.jpg">
+        </a>
+        <div class="content">
+          <a class="author">Matt</a>
+          <div class="metadata">
+            <span class="date">Today at 5:42PM</span>
+          </div>
+          <div class="text">
+            How artistic!
+          </div>
+          <div class="actions">
+            <a class="reply">Reply</a>
+          </div>
+        </div>
+      </div>
+      <div class="comment">
+        <a class="avatar">
+          <img src="/images/avatar/small/elliot.jpg">
+        </a>
+        <div class="content">
+          <a class="author">Elliot Fu</a>
+          <div class="metadata">
+            <span class="date">Yesterday at 12:30AM</span>
+          </div>
+          <div class="text">
+            <p>This has been very useful for my research. Thanks as well!</p>
+          </div>
+          <div class="actions">
+            <a class="reply">Reply</a>
+          </div>
+        </div>
+        <div class="comments">
+          <div class="comment">
+            <a class="avatar">
+              <img src="/images/avatar/small/jenny.jpg">
+            </a>
+            <div class="content">
+              <a class="author">Jenny Hess</a>
+              <div class="metadata">
+                <span class="date">Just now</span>
+              </div>
+              <div class="text">
+                Elliot you are always so right :)
+              </div>
+              <div class="actions">
+                <a class="reply">Reply</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="comment">
+        <a class="avatar">
+          <img src="/images/avatar/small/joe.jpg">
+        </a>
+        <div class="content">
+          <a class="author">Joe Henderson</a>
+          <div class="metadata">
+            <span class="date">5 days ago</span>
+          </div>
+          <div class="text">
+            Dude, this is awesome. Thanks so much
+          </div>
+          <div class="actions">
+            <a class="reply">Reply</a>
+          </div>
+        </div>
+      </div>
+      <form class="ui reply form">
+        <div class="field">
+          <textarea></textarea>
+        </div>
+        <div class="ui blue labeled submit icon button">
+          <i class="icon edit"></i> Add Reply
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  data () {
+    return {
+      'id': 1,
+      'created_at': '2017-03-16T21:25:22.739516',
+      'updated_at': '2017-03-16T21:25:22.739573',
+      'deleted_at': null,
+      'content': 'test',
+      'is_anonymous': false,
+      'is_content_erotic': false,
+      'is_content_social': false,
+      'use_signature': false,
+      'title': 'test',
+      'comments': [
+        {
+          'id': 1,
+          'created_at': '2017-03-16T21:25:22.739516',
+          'updated_at': '2017-03-16T21:25:22.739573',
+          'deleted_at': null,
+          'content': 'test',
+          'is_anonymous': false,
+          'is_content_erotic': false,
+          'is_content_social': false,
+          'use_signature': false
+        },
+        {
+          'id': 1,
+          'created_at': '2017-03-16T21:25:22.739516',
+          'updated_at': '2017-03-16T21:25:22.739573',
+          'deleted_at': null,
+          'content': 'test',
+          'is_anonymous': false,
+          'is_content_erotic': false,
+          'is_content_social': false,
+          'use_signature': false
+        },
+        {
+          'id': 1,
+          'created_at': '2017-03-16T21:25:22.739516',
+          'updated_at': '2017-03-16T21:25:22.739573',
+          'deleted_at': null,
+          'content': 'test',
+          'is_anonymous': false,
+          'is_content_erotic': false,
+          'is_content_social': false,
+          'use_signature': false
+        },
+        {
+          'id': 1,
+          'created_at': '2017-03-16T21:25:22.739516',
+          'updated_at': '2017-03-16T21:25:22.739573',
+          'deleted_at': null,
+          'content': 'test',
+          'is_anonymous': false,
+          'is_content_erotic': false,
+          'is_content_social': false,
+          'use_signature': false
+        },
+        {
+          'id': 1,
+          'created_at': '2017-03-16T21:25:22.739516',
+          'updated_at': '2017-03-16T21:25:22.739573',
+          'deleted_at': null,
+          'content': 'test',
+          'is_anonymous': false,
+          'is_content_erotic': false,
+          'is_content_social': false,
+          'use_signature': false
+        }
+      ],
+      'categories': [
+        1
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add 'scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- ara/src/router/index.js
+++ ara/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Article from '@/components/Article'
+import ArticleCreate from '@/components/ArticleCreate'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Article',
+      component: Article
+    },
+    {
+      path: '/',
+      name: 'ArticleCreate',
+      component: ArticleCreate
     }
   ]
 })
